483d46c8bca12ee77c52fcf73ea281c1fae8e51f
akash87123 <akash.ola2014@gmail.com>
2025-11-21 20:30:39 -0500

    Update command.h

--- command.h
+++ command.h
@@ -1,1 +1,24 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include <stdint.h>
+
+typedef enum {
+    CMD_INSERT,
+    CMD_DELETE,
+    CMD_UPDATE,
+    CMD_SEARCH,
+    CMD_PRINT,
+    CMD_NONE
+} CommandType;
+
+typedef struct {
+    CommandType type;
+    char name[64];
+    uint32_t salary;
+    int thread_id;  
+    int valid;
+} Command;
+
+#endif 
 
