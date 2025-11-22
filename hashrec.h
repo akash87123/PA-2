52293a2c9a2eb1ffb1f23df50517522b1f493571
akash87123 <akash.ola2014@gmail.com>
2025-11-21 20:30:07 -0500

    Update hashrec.h

--- hashrec.h
+++ hashrec.h
@@ -1,1 +1,16 @@
+#ifndef HASHREC_H
+#define HASHREC_H
+
+#include <stdint.h>
+
+#define MAX_NAME_LEN 50
+
+typedef struct hash_struct {
+    uint32_t hash;
+    char     name[MAX_NAME_LEN + 1];
+    uint32_t salary;
+    struct hash_struct *next;
+} hashRecord;
+
+#endif
 
